bfc162073c230c4ca13a6b1676b512f6a7a0807d
Fathima Haris <137689640+fvthima@users.noreply.github.com>
2025-03-22 02:47:42 +0400

    Update config.py

--- Documents/uni/321/tutoreal 2/config.py
+++ Documents/uni/321/tutoreal 2/config.py
@@ -1,10 +1,10 @@
 # config.py
 from urllib.parse import quote_plus
 
-DB_USERNAME = 'root'
-DB_PASSWORD = quote_plus('Fathimaharis@1')
-DB_HOST = 'localhost'
-DB_NAME = 'tutoreal'  # must match the database name
+DB_USERNAME = ''
+DB_PASSWORD = ''
+DB_HOST = ''
+DB_NAME = ''  # must match the database name
 
 SQLALCHEMY_DATABASE_URI = (
     f"mysql+mysqlconnector://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}?charset=utf8"
